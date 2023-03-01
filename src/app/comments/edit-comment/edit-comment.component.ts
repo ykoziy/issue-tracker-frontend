3a67c4f784a3918b40a5914017e5d8270d385c48
Yuriy Koziy <yukoziy@gmail.com>
2023-02-28 21:32:52 -0500

    added confirmation for deleting comment

--- src/app/comments/edit-comment/edit-comment.component.ts
+++ src/app/comments/edit-comment/edit-comment.component.ts
@@ -1,10 +1,13 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { LoginService } from 'src/app/auth/login.service';
 import { Comment } from 'src/app/interfaces/comment';
 import { CommentService } from 'src/app/service/comment.service';
+import { AnchorDirective } from 'src/app/shared/modal/anchor.directive';
+import { ConfirmComponent } from 'src/app/shared/modal/confirm/confirm.component';
 
 @Component({
   selector: 'app-edit-comment',
@@ -14,6 +17,11 @@ import { CommentService } from 'src/app/service/comment.service';
 export class EditCommentComponent implements OnInit {
   comment!: Comment;
   editCommentForm = {} as FormGroup;
+  @ViewChild(AnchorDirective, { static: false })
+  modalHost!: AnchorDirective;
+  private cancelSub!: Subscription;
+  private okSub!: Subscription;
+  private message = 'This will update the comment. Are you sure?';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,18 +41,42 @@ export class EditCommentComponent implements OnInit {
     });
   }
 
+  saveComment(): void {
+    const updateComment: Comment = { ...this.comment };
+    updateComment.content = this.editCommentForm.value.userComment;
+    const userId = this.loginService.getUserId();
+    if (userId != 0) {
+      this.commentService.editComment(userId, updateComment).subscribe({
+        next: () => {
+          this.router.navigate(['/issue', this.comment.issueId]);
+        },
+      });
+    }
+  }
+
   onSubmit(): void {
     if (this.editCommentForm.valid) {
-      const updateComment: Comment = { ...this.comment };
-      updateComment.content = this.editCommentForm.value.userComment;
-      const userId = this.loginService.getUserId();
-      if (userId != 0) {
-        this.commentService.editComment(userId, updateComment).subscribe({
-          next: () => {
-            this.router.navigate(['/issue', this.comment.issueId]);
-          },
-        });
-      }
+      this.showConfirmation();
     }
+  }
+
+  showConfirmation(): void {
+    const confirmRef =
+      this.modalHost.viewContainerRef.createComponent<ConfirmComponent>(
+        ConfirmComponent
+      );
+
+    confirmRef.instance.message = this.message;
+
+    this.cancelSub = confirmRef.instance.cancel.subscribe(() => {
+      this.cancelSub.unsubscribe();
+      confirmRef.destroy();
+    });
+
+    this.okSub = confirmRef.instance.ok.subscribe(() => {
+      this.saveComment();
+      this.okSub.unsubscribe();
+      confirmRef.destroy();
+    });
   }
 }
