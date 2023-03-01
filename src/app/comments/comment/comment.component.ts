ba39398803c39afe5ffa0a9293e6fee871ddacbb
Yuriy Koziy <yukoziy@gmail.com>
2023-02-28 21:38:12 -0500

    added confirmation for deleting comment

--- src/app/comments/comment/comment.component.ts
+++ src/app/comments/comment/comment.component.ts
@@ -1,8 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { LoginService } from 'src/app/auth/login.service';
 import { Comment } from 'src/app/interfaces/comment';
 import { CommentService } from 'src/app/service/comment.service';
+import { AnchorDirective } from 'src/app/shared/modal/anchor.directive';
+import { ConfirmComponent } from 'src/app/shared/modal/confirm/confirm.component';
 
 @Component({
   selector: 'app-comment',
@@ -16,6 +26,12 @@ export class CommentComponent implements OnInit {
   @Input() comment!: Comment;
 
   userId: number = 0;
+
+  @ViewChild(AnchorDirective, { static: false })
+  modalHost!: AnchorDirective;
+  private cancelSub!: Subscription;
+  private okSub!: Subscription;
+  private message = 'This will delete the comment. Are you sure?';
 
   constructor(
     private commentService: CommentService,
@@ -35,13 +51,15 @@ export class CommentComponent implements OnInit {
 
   onDelete(): void {
     if (this.userId !== 0) {
-      this.router.navigate(['/issue', this.comment.issueId]);
-      this.commentService
-        .deleteComment(this.userId, this.comment.id)
-        .subscribe({
-          next: () => this.commentModifiedEvent.emit(),
-        });
+      this.showConfirmation();
     }
+  }
+
+  deleteComment(): void {
+    this.router.navigate(['/issue', this.comment.issueId]);
+    this.commentService.deleteComment(this.userId, this.comment.id).subscribe({
+      next: () => this.commentModifiedEvent.emit(),
+    });
   }
 
   isUserComment(authorId: number): boolean {
@@ -54,5 +72,25 @@ export class CommentComponent implements OnInit {
       return true;
     }
     return false;
+  }
+
+  showConfirmation(): void {
+    const confirmRef =
+      this.modalHost.viewContainerRef.createComponent<ConfirmComponent>(
+        ConfirmComponent
+      );
+
+    confirmRef.instance.message = this.message;
+
+    this.cancelSub = confirmRef.instance.cancel.subscribe(() => {
+      this.cancelSub.unsubscribe();
+      confirmRef.destroy();
+    });
+
+    this.okSub = confirmRef.instance.ok.subscribe(() => {
+      this.deleteComment();
+      this.okSub.unsubscribe();
+      confirmRef.destroy();
+    });
   }
 }
